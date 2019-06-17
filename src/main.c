a9017d601210e347bc201f45feb5f017d51d47a2
Victor J. Kartsch <javier_kartsch@hotmail.com>
2019-06-17 16:09:34 +0200

    Initial Commit

---
+++ src/main.c
@@ -0,0 +1,33 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  Ac6
+  * @version V1.0
+  * @date    01-December-2013
+  * @brief   Default main function.
+  ******************************************************************************
+*/
+
+
+#include "stm32f10x.h"
+#include "stm32f1xx_nucleo.h"
+#include "MPU6050.h"
+#include "tim2_delay.h"
+			
+
+int main(void)
+{
+	int16_t data_acc[20];
+
+	delay_init();
+	MPU6050_I2C_Init();
+
+	if(MPU6050_TestConnection())
+			MPU6050_Initialize();
+
+	for(;;)
+	{
+		MPU6050_GetRawAccelGyro(data_acc);
+		delay_ms(100);
+	}
+}
